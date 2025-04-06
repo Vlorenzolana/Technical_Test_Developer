9edfc94dd1c8df925c9db13cc6f874dcacab886a
raspi <vlorenzolana@gmail.com>
2025-04-06 10:21:03 +0200

    added structs

--- inc/addnum.h
+++ inc/addnum.h
@@ -14,4 +14,19 @@
 #define MAX_LINE 128
 #define THREAD_LIMIT 100
 
+typedef struct s_list {
+    int *numbers;
+    int load_size;
+    int count;
+    pthread_rwlock_t lock;
+} t_list;
+
+typedef struct s_threadData {
+    int thread_id;
+    int num_per_thread;
+} t_threadData;
+
+extern t_list add_even;
+extern t_list add_odd;
+
 #endif
